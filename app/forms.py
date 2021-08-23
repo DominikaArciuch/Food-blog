6e00ce66ab1d03d976c33e0b0adc0373698e3951
Unilady <arcdom703@gmail.com>
2021-08-24 00:23:43 +0200

    Finished register page

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email adress", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), EqualTo("submit_password", message='Passwords must match')])
+    submit_password = PasswordField("Repeat password")
+    sign_in = SubmitField("Submit", validators=[DataRequired()])
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember me", validators=[DataRequired()])
     sign_in = SubmitField("Sign in", validators=[DataRequired()])
-
