1d3df45377cb731a85298d98dba927f29d9b940f
Unilady <arcdom703@gmail.com>
2021-08-24 23:28:33 +0200

    Create database and migrations files, create accounts table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Accounts(db.Model):
@@ -9,3 +11,19 @@ class Accounts(db.Model):
 
     def __repr__(self):
         return 'id {}, User {}, email {}'.format(self.id, self.username, self.email)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
